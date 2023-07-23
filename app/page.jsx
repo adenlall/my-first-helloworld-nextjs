7365870c705e199d72943f8d7fb1c33fa5f51fc3
aden lall <91727676+adenlall@users.noreply.github.com>
2023-07-23 12:52:23 +0000

    fixes

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,14 @@
-import '@styles/index.css';
+import "@styles/index.css";
 
 export const metadata = {
-	title : "this is salllllllllllatokk";
-}
+  title: "this is salllllllllllatokk",
+};
 
 export default function Page() {
-
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<h1 className="text-2xl font-extrabold text-blue-800">Page::Home </h1>
-			<h2 className="text-red-300">welcome to salatok-nextjs</h2>
-		</main>
-	)
-
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-2xl font-extrabold text-blue-800">Page::Home </h1>
+      <h2 className="text-red-300">welcome to salatok-nextjs</h2>
+    </main>
+  );
 }
